8e25254be21be3d98a144078098069ea9cdf8cbc
Carlos Vivas <65358778+cabjkiko@users.noreply.github.com>
2023-04-10 17:08:36 -0300

    Finished all the exercises from 1 to 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  var celsius = (fahrenheit - 32) * 5 / 9;
+  return Math.round(celsius * 10) / 10; // round to 1 decimal
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  var fahrenheit = (celsius * 9 / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10; // round to 1 decimal
 };
 
 // Do not edit below this line
